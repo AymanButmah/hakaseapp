5474020cb1d77c459eec2c9cb28d513990d0342c
AymanButmah <Prince.butmah@outlook.com>
2021-06-28 23:06:57 +0300

    CRUD Scaffold deployed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  resources :friends
   get 'home/friends'
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
-  get "home/events"
 end
